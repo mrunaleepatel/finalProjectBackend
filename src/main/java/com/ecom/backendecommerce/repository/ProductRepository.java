51e3f7ad0283601a910f57352d6585434c822a5c
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-10-11 02:14:59 -0500

    version change

--- src/main/java/com/ecom/backendecommerce/repository/ProductRepository.java
+++ src/main/java/com/ecom/backendecommerce/repository/ProductRepository.java
@@ -3,7 +3,6 @@ package com.ecom.backendecommerce.repository;
 import com.ecom.backendecommerce.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
