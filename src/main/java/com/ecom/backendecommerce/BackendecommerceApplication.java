37fa9db8fae730afd4600cf549d020a8949bc74b
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-10-16 20:35:32 -0500

    data change

--- src/main/java/com/ecom/backendecommerce/BackendecommerceApplication.java
+++ src/main/java/com/ecom/backendecommerce/BackendecommerceApplication.java
@@ -3,11 +3,13 @@ package com.ecom.backendecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @CrossOrigin(origins = "http://localhost:4200")
 @Configuration
+@PropertySource("classpath:application.properties")
 public class BackendecommerceApplication {
 
 	public static void main(String[] args) {
