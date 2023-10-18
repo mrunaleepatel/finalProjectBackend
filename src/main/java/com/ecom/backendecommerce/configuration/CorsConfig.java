2f3507125b78ba21ead60bc004ee8dea52b09062
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-10-18 15:01:47 -0500

    cors origin

--- src/main/java/com/ecom/backendecommerce/configuration/CorsConfig.java
+++ src/main/java/com/ecom/backendecommerce/configuration/CorsConfig.java
@@ -9,7 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://652edd312260c35e5ff1ebda--dazzling-caramel-a8189f.netlify.app/") // Replace with the origin of your Angular app
+//                .allowedOrigins("https://652edd312260c35e5ff1ebda--dazzling-caramel-a8189f.netlify.app/") // Replace with the origin of your Angular app
+                .allowedOrigins("https://main--fascinating-pika-3500f2.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
